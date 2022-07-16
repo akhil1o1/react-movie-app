6acdbbe2d2565378b0b9f912724ef9ea09b41821
akhil panwar <akhilpanwar180@gmail.com>
2022-07-16 22:24:48 +0530

    started movie app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Hero from "./components/Hero";
+import {Box} from "@mui/material";
+
+function App (){
+    return <Box>
+        <Hero/>
+    </Box>
+}
+
+export default App;
---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Paper} from "@mui/material";
+
+function Hero(){
+    return <Paper sx={{height:"30vh"}} variant="outlined" square>
+        <h1>welcome to movie app</h1>
+    </Paper>
+}
+
+export default Hero;
